318981d6947c6fae9a6854433423e43b0577c101
matata samuel <mattasamuel3@gmail.com>
2020-08-22 09:24:42 +0300

    added input property binding component

--- Diary/src/app/app.module.ts
+++ Diary/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { DiaryComponent } from './diary/diary.component';
+import { DiaryDetailComponent } from './diary-detail/diary-detail.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    DiaryComponent
+    DiaryComponent,
+    DiaryDetailComponent
   ],
   imports: [
     BrowserModule,
